f278fc26181bfff839e2f1ae1293e49d039d986b
zum-m <21039m@gmail.com>
2021-10-15 22:59:23 +0900

    10.4 viewファイルの＠foreach ($tweets as $tweet)の tweetsの方がコントローラーから渡せれてくる方

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -21,7 +21,9 @@
               <tbody>
                 <!-- 10.4$tweetsはコントローラーから渡される予定のもの 渡ってくるもの-->
                 <!-- p７８繰り返しのディレクティブ @foreach（$配列 as $変数） -->
-                @foreach ($tweets as $tweet) 
+                <!-- このしたの$wteetはコントローラーから渡されるよでいでない時は $tweets is undifinedと出る -->
+                @foreach ($tweets as $tweet)  
+                
                 <tr class="hover:bg-grey-lighter">
                   <td class="py-4 px-6 border-b border-grey-light">
                     <h3 class="text-left font-bold text-lg text-grey-dark">{{$tweet->tweet}}</h3>
