4acc3999d0eaeccba602dca0794784dc5fb3b031
zum-m <21039m@gmail.com>
2021-10-15 22:49:02 +0900

    10.4　エラー処理は共通のviewを作る バリデーターは？？それとの違いは？？viewであること？

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,4 +1,16 @@
-<!-- 10.4フォルダ・ファイル追加 -->
+<!-- 10.4フォルダ・ファイル追加  10.4 -->
+
+<!-- 共通パーツ（エラー表示）の作成
+入力値が不正な場合などはエラー画面を表示して対応する．
+
+tweet の入力や編集など複数の画面で必要となるため，共通のコンポーネントとして作成する．
+
+latavel_tweet/resources/viewsの中にcommonフォルダを作成する． commonフォルダの中にerrors.blade.phpファイルを作成する．
+
+errors.blade.phpに以下の内容を記述する． -->
+
+
+
   @if (count($errors) > 0)
   <div>
     <div class="font-medium text-red-600">
